1d761c15ffc45cab29e850429c1990c5b7aa00b5
gedeastu <gede.astu@gmail.com>
2024-02-10 14:50:21 +0800

    splash screen is done

--- app/src/main/java/com/example/splashscreen/MainActivity.kt
+++ app/src/main/java/com/example/splashscreen/MainActivity.kt
@@ -1,16 +1,34 @@
 package com.example.splashscreen
 
 import android.os.Bundle
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.splashscreen.ui.theme.SplashScreenTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +40,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    Navigation()
                 }
             }
         }
@@ -30,17 +48,53 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Navigation(){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "splash_screen"){
+        composable("splash_screen"){
+            SplashScreen(navController = navController)
+        }
+        composable("home_screen"){
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+                Text(text = "WELCOME BACK!")
+            }
+        }
+    }
 }
+
+@Composable
+fun SplashScreen(navController: NavController){
+    val scale = remember {
+        androidx.compose.animation.core.Animatable(
+            0f
+        )
+    }
+    LaunchedEffect(key1 = Unit, block = {
+        scale.animateTo(
+            targetValue = 0.3f,
+            animationSpec = tween(
+                durationMillis = 500,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            )
+        )
+        delay(2000L)
+        navController.navigate("home_screen")
+    })
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize().background(Color.Gray),){
+        Image(modifier = Modifier.scale(scale.value).clip(CircleShape),painter = painterResource(id = R.drawable.mypict), contentDescription = null)
+    }
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SplashScreenTheme {
-        Greeting("Android")
+
     }
 }
